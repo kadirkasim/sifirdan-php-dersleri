26261adcfa8fd173584c5fd8859825d8825f8620
Kadir Kasim <kadirkasim@hotmail.com>
2018-07-13 01:38:23 -0400

    index.php olusturuldu

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+  // Tek satır acıklama yazısı
+  # Tek satır acıklama yazısı
+
+  /*
+    Çoklu
+    Satır
+    Açıklama
+    Yazısı
+  */
+
+  echo "<h2>Sıfırdan Php Dersleri</h2>";
+
+?>
