5f6f7b1b3e7991a0472afa146118c12dc81e8da0
Kadir Kasim <kadirkasim@hotmail.com>
2018-07-15 04:29:54 -0400

    Aritmatiksel Operatörler

---
+++ operatorler-1.php
@@ -0,0 +1,65 @@
+<?php
+
+  /*
+
+    Aritmatiksel Operatorler
+
+      + Toplama
+      - Cikarma
+      * Carpma
+      / Bolme
+      % Mod
+
+      += Toplar ve atama
+      -= Cikartir ve atama
+      *= Carpma ve atama
+      /= Bolme ve atama
+      %= Mod ve atama
+
+      $a++ sonra arttirir
+      ++$a once arttirir
+
+      $a-- sonra eksiltir
+      --$a once eksiltir
+
+  */
+
+  $a = 5;
+  $b = 9;
+
+  echo $a + $b. "<br>";
+  echo $a - $b. "<br>";
+  echo $a * $b. "<br>";
+  echo $a / $b. "<br>";
+  echo $a % $b. "<br>";
+
+  $a += 5;
+  echo $a ."<br>";
+
+  $a -= 5;
+  echo $a ."<br>";
+
+  $a *= 5;
+  echo $a ."<br>";
+
+  $a /= 5;
+  echo $a ."<br>";
+
+  $a %= 5;
+  echo $a ."<br>";
+
+  echo 5 + 4 * 2 . "<br>";
+
+  echo (5 + 4) * 2;
+
+  echo "<br>";
+  echo "<br>";
+  echo "<br>";
+
+  $c = 10;
+  $c++;
+  echo $c. "<br>";
+  $c--;
+  echo $c. "<br>";
+
+?>
