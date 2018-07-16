aa738df9af559a9db5fe899ecad8e585d4d9081a
Kadir Kasim <kadirkasim@hotmail.com>
2018-07-16 01:32:07 -0400

    Karşılaştırma Operatörleri

---
+++ operatorler-2.php
@@ -0,0 +1,56 @@
+<?php
+
+  /*
+
+    Karsilastirma Operatorleri
+
+      == esitse
+      != esit degilse
+      > Buyukse
+      < kucukse
+      >= buyuk esitse
+      <= kucuk esitse
+      === denkse
+      !== denk degilse
+
+    Mantik Operatorleri
+
+      && AND VE
+      \\ OR VEYA
+      ! degil
+
+  */
+
+
+  $a = 8;
+  $b = 3;
+  $c = 3;
+
+  // echo $a == $b;
+  // echo $a != $b;
+  // echo $a > $b;
+  // echo $a < $b;
+  // echo $a >= $b;
+  // echo $a <= $b;
+
+  // echo $a === "8";
+  // echo $a !== "8";
+
+  // echo ($a == 8) && ($b == 3); // true
+  // echo ($a == 8) && ($b == 5); // false
+  // echo ($a == 9) && ($b == 3); // false
+  // echo ($a == 9) && ($b == 5); // false
+
+
+  // echo ($a == 8) || ($b == 3); // true
+  // echo ($a == 8) || ($b == 5); // true
+  // echo ($a == 9) || ($b == 3); // true
+  // echo ($a == 9) || ($b == 5); // false
+
+
+  // echo ($a - 5 == 3);
+
+
+
+
+?>
