dd6873e1390d68893e39ed1dad3f4ea2d489cb55
Kadir Kasim <kadirkasim@hotmail.com>
2018-07-18 03:10:54 -0400

    Switch Case Kosul Ifadesi

---
+++ kosulifadeleri-2.php
@@ -0,0 +1,33 @@
+<?php
+
+  // $fav = 'kirmizi';
+  //
+  // switch($fav){
+  //   case 'kirmizi':
+  //     echo "favori renginiz kirmizidir";
+  //   break;
+  //
+  //   case 'mavi':
+  //     echo "favori renginiz mavidir";
+  //   break;
+  //
+  //   default:
+  //     echo "favori renginiz tanimli degildir";
+  //   break;
+  // }
+
+
+  $not = 1;
+
+  switch($not):
+    case $not > 2:
+      echo "basarili";
+    break;
+
+    default:
+      echo "tanimli degildir";
+    break;
+  endswitch;
+
+
+?>
