4826e975b57a5ef8689f0de861bd48b40343eb7f
Kadir Kasim <kadirkasim@hotmail.com>
2018-07-29 01:54:39 -0400

    String Fonksiyonlari

---
+++ fonksiyonlar-3.php
@@ -0,0 +1,110 @@
+<?php
+
+  /*
+    Sihirli Karakterler
+      \t
+      \n
+      \$
+      \"
+      \'
+      \\
+  */
+
+  /*
+    String Fonksiyonlari
+
+      strlen -> uzunlugunu dondurur.
+      strstr -> arama
+      strpos -> arama
+      str_replace -> degisiklik
+      str_repeat -> tekrarlatma
+      strtoupper, strtolower
+      ucwords, ucfirst
+
+      explode -> parcalar
+      implode -> birlestirir
+
+      trim , ltrim , rtrim
+
+  */
+
+  // echo "\$\tstr\n yazi : \"bu bir alintidir.\"  \\t";
+  $str = "bu bir string degerdir.";
+  $email = "kadir@example.com";
+
+  // echo strlen($str);
+  // echo strstr($str , 'bir');
+  // echo strstr($email, '@', true);
+  // echo strpos($str , 'b');
+  // echo str_replace(["bu","string"],['o','integer'],$str);
+  // echo str_repeat('*',5);
+
+  // $str2 = strtoupper($str);
+  // echo strtolower($str2);
+
+  // echo ucwords($str);
+  // echo ucfirst($str);
+
+  // $arr = explode(' ',$str);
+  // echo "<pre>";
+  // print_r($arr);
+  // echo "</pre>";
+
+  // echo implode(',',$arr);
+
+  // $str3 = "\t\tdeneme\t\t";
+  // echo trim($str3);
+  // echo ltrim($str3);
+  // echo rtrim($str3);
+
+  $str4 = "-deneme-";
+  // echo trim($str4,'-');
+  // echo ltrim($str4,'-');
+  // echo rtrim($str4,'-');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
