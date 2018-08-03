66c4be80726dd45a05296f2070ba866abdb08f65
Kadir Kasim <kadirkasim@hotmail.com>
2018-08-03 04:38:35 -0400

    Dosya Cagirma

---
+++ dosyalar-1.php
@@ -0,0 +1,21 @@
+<?php
+
+  /*
+    include
+    include_once
+
+    require
+    require_once
+  */
+
+  include 'fonksiyonlar-1.php';
+  require 'degiskenler.php';
+
+  for ($i=0; $i < 5; $i++) {
+    include 'index.php';
+  }
+
+  // echo faktoriyel(5);
+  echo $str;
+
+?>
